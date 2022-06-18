f8fed76fb47b998e455033d743c0bc0be6123b6f
dolphinxx <bravedolphinxx@gmail.com>
2022-06-18 23:31:51 +0800

    fix issues on iOS/MacOS after refactor

--- flutter_opencc_ffi_ios/ios/Classes/FlutterOpenccFfiPlugin.m
+++ flutter_opencc_ffi_ios/ios/Classes/FlutterOpenccFfiPlugin.m
@@ -1,11 +1,11 @@
 #import "FlutterOpenccFfiPlugin.h"
-#if __has_include(<flutter_opencc_ffi/flutter_opencc_ffi-Swift.h>)
-#import <flutter_opencc_ffi/flutter_opencc_ffi-Swift.h>
+#if __has_include(<flutter_opencc_ffi_ios/flutter_opencc_ffi_ios-Swift.h>)
+#import <flutter_opencc_ffi_ios/flutter_opencc_ffi_ios-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "flutter_opencc_ffi-Swift.h"
+#import "flutter_opencc_ffi_ios-Swift.h"
 #endif
 
 @implementation FlutterOpenccFfiPlugin
